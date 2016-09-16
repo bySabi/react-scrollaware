bf458fcaf5697d0ccf7a5fa87bd824880cc06856
bySabi Files <bysabi.files@gmail.com>
2016-09-16 14:27:24 +0200

    update travis

--- karma.conf.js
+++ karma.conf.js
@@ -2,4 +2,7 @@ const carma = require('carma-tap-webpack');
 
 module.exports = function(config) {
   carma(config);
+  config.set({
+    browsers: process.env.CONTINUOUS_INTEGRATION === 'true' ? ['Chrome_ci'] : ['Chrome']
+  });
 }
