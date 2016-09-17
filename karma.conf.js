ffbedea5bc3d79626dd8544f4bb454d0d9d15eef
bySabi Files <bysabi.files@gmail.com>
2016-09-17 23:21:00 +0200

    update carma-tap-webpack

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,4 @@ const carma = require('carma-tap-webpack');
 
 module.exports = function(config) {
   carma(config);
-  config.set({
-    browsers: process.env.CONTINUOUS_INTEGRATION === 'true' ? ['Chrome_ci'] : ['Chrome']
-  });
 }
